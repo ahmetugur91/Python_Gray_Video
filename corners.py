fe942cfb8a3a23ae717613d90b5cc26b0eca6f19
Ahmet UGUR <ahmet.ugur@std.gau.edu.tr>
2019-04-04 02:51:55 +0300

    kağıt tespit etme kamera

--- corners.py
+++ corners.py
@@ -2,16 +2,20 @@ import numpy as np
 import cv2
 
 
-img = cv2.imread('just_board.jpg')
+img = cv2.imread('board.jpg')
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 gray = np.float32(gray)
 
 corners = cv2.goodFeaturesToTrack(gray, 100, 0.01, 10)
 corners = np.int0(corners)
 
+key = 0
 for corner in corners:
     x, y = corner.ravel()
     cv2.circle(img, (x, y), 3, 255, -1)
+
+    cv2.putText(img, str(key), (x+5, y+5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0))
+    key = key + 1
 
 cv2.imshow('Corner', img)
 
