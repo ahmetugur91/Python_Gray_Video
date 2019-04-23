b32d2fcaa7643252b3fd5a4062352122c6043037
Ahmet UGUR <ahmet.ugur@std.gau.edu.tr>
2019-04-23 23:38:33 +0300

    center dots

--- negative.py
+++ negative.py
@@ -1,4 +1,5 @@
 import math
+import copy
 import numpy as np
 import cv2
 
@@ -27,9 +28,16 @@ def line_intersection(line1, line2):
     return x, y
 
 
+showCornerPoints = False
+showIndexNumbers = False
+showCenterDots = True
+drawLines = False
+drawBorders = True
+
 # load the image
-image = cv2.imread("4.jpg", 1)
+image = cv2.imread("1.jpg", 1)
 image = cv2.resize(image, (500, 800))
+image_original = copy.copy(image)
 # white color boundaries (R,B and G)
 lower = [120, 120, 120]
 upper = [255, 255, 255]
@@ -50,6 +58,9 @@ thresh = cv2.medianBlur(thresh, 11)
 # cv2.imshow("",thresh)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
+intersectionPoints = []
+boxes = []
+
 if len(contours) != 0:
     # draw in blue the contours that were founded
     c = max(contours, key=cv2.contourArea)
@@ -64,10 +75,11 @@ if len(contours) != 0:
     point3 = (approx[3][0][0], approx[3][0][1])
     point4 = (approx[2][0][0], approx[2][0][1])
 
-    cv2.circle(image, point1, 5, (0, 0, 255), -1)
-    cv2.circle(image, point2, 5, (0, 0, 255), -1)
-    cv2.circle(image, point3, 5, (0, 0, 255), -1)
-    cv2.circle(image, point4, 5, (0, 0, 255), -1)
+    if showCornerPoints:
+        cv2.circle(image, point1, 5, (0, 0, 255), -1)
+        cv2.circle(image, point2, 5, (0, 0, 255), -1)
+        cv2.circle(image, point3, 5, (0, 0, 255), -1)
+        cv2.circle(image, point4, 5, (0, 0, 255), -1)
 
     # cv2.line(image, point1, point2, 255, 5)
     # cv2.line(image, point3, point4, 255, 5)
@@ -102,18 +114,26 @@ if len(contours) != 0:
     rightPieceLenX = roundto((point4[0] - point3[0]) / 15)
     rightPieceLenY = roundto((point4[1] - point3[1]) / 15)
 
-    for xrow in range(1, 15, 1):
+    for yrow in range(0, 16, 1):
+        pointDotTop = (point1[0] + yrow * topPieceLenX, point1[1] + yrow * topPieceLenY)
+        intersectionPoints.append(pointDotTop)
+        cv2.circle(image, pointDotTop, 1, (255, 0, 0), -1)
+
+    for xrow in range(1, 16, 1):
         # print(i)
         pointDotLeft = (point1[0] + xrow * leftPieceLenX, point1[1] + xrow * leftPieceLenY)
         pointDotRight = (point3[0] + xrow * rightPieceLenX, point3[1] + xrow * rightPieceLenY)
 
-        for yrow in range(1,15,1):
+        intersectionPoints.append(pointDotLeft)
+        cv2.circle(image, pointDotLeft, 1, (255, 0, 0), -1)
+
+        for yrow in range(1, 15, 1):
             pointDotTop = (point1[0] + yrow * topPieceLenX, point1[1] + yrow * topPieceLenY)
             pointDotBottom = (point2[0] + yrow * bottomPieceLenX, point2[1] + yrow * bottomPieceLenY)
 
             # print(pointDotTop, pointDotBottom)
-            # cv2.line(image, pointDotTop, pointDotBottom, 255, 1)
-            # cv2.line(image, pointDotLeft, pointDotRight, 255, 1)
+            cv2.line(image, pointDotTop, pointDotBottom, 255, 1)
+            cv2.line(image, pointDotLeft, pointDotRight, 255, 1)
 
             cv2.circle(image, pointDotTop, 1, (0, 0, 255), -1)
             cv2.circle(image, pointDotBottom, 1, (0, 0, 255), -1)
@@ -121,8 +141,12 @@ if len(contours) != 0:
             cv2.circle(image, pointDotRight, 1, (0, 0, 255), -1)
 
             x, y = line_intersection((pointDotTop, pointDotBottom), (pointDotLeft, pointDotRight))
-            cv2.circle(image, (roundto(x), roundto(y)), 1, (255, 0 , 0), -1)
-            print(x, y)
+            cv2.circle(image, (roundto(x), roundto(y)), 1, (255, 0, 0), -1)
+            intersectionPoints.append((roundto(x), roundto(y)))
+            # print(x, y)
+
+        intersectionPoints.append(pointDotRight)
+        cv2.circle(image, pointDotRight, 1, (255, 0, 0), -1)
 
     # middleTopX = int((point1[0] + point3[0]) / 2)
     # middleTopY = int((point1[1] + point3[1]) / 2)
@@ -146,7 +170,41 @@ if len(contours) != 0:
 
 # show the images
 # cv2.imshow("Result", np.hstack([image, output]))
-cv2.imshow("Original", image)
+
+# for index numbers on image
+counter = 0
+for point in intersectionPoints:
+    print(point)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    fontScale = 0.3
+    fontColor = (0, 255, 255)
+    lineType = 1
+
+    cv2.putText(image, str(counter),
+                point,
+                font,
+                fontScale,
+                fontColor,
+                lineType)
+
+    counter = counter + 1
+
+for row in range(0, 15, 1):
+    for col in range(0, 15, 1):
+        current = col + row * 16
+        point1 = intersectionPoints[current]
+        point2 = intersectionPoints[current + 17]
+        # cv2.line(image, point1, point2, 200, 1)
+        middleX = int((point1[0] + point2[0]) / 2)
+        middleY = int((point1[1] + point2[1]) / 2)
+        cv2.circle(image, (middleX, middleY), 1, (255, 0, 0), -1)
+
+# cropped = image_original[100:200, 140:440]
+# cv2.imshow("cropped", cropped)
+
+
+cv2.imshow('Processed', image)
 # cv2.imshow("Cropped", output)
 
 cv2.waitKey(0)
