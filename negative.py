d1bc53d1a3af41e0c38d8d023d35babe626c9538
ahmetugur91 <carnack54@gmail.com>
2019-05-19 01:57:15 +0300

    kırmızı noktalar ve 7 noktadan renk seçimi

--- negative.py
+++ negative.py
@@ -2,6 +2,7 @@ import math
 import copy
 import numpy as np
 import cv2
+from collections import Counter
 
 
 def roundto(number):
@@ -28,6 +29,34 @@ def line_intersection(line1, line2):
     return x, y
 
 
+def color_from_rgb(r, g, b):
+    if r < 50 and g < 50 and b > 125:
+        return "blue"
+    if r < 50 and g > 125 and b < 50:
+        return "green"
+    if r > 120 and g < 50 and b < 50:
+        return "red"
+    if r > 120 and g > 120 and b > 120:
+        return "white"
+
+
+def player_decision(r, g, b):
+    if color_from_rgb(r, g, b) == "green":
+        return 1
+    if color_from_rgb(r, g, b) == "blue":
+        return 2
+    if color_from_rgb(r, g, b) == "white":
+        return 0
+    return -1
+
+
+r = 237
+g = 27
+b = 35
+
+clr = color_from_rgb(r, g, b)
+player = player_decision(r, g, b)
+
 showPointDots = False
 showCornerPoints = True
 showIndexNumbers = False
@@ -37,7 +66,7 @@ showBorders = True
 showIntersectionPoints = False
 
 # load the image
-image = cv2.imread("4.jpg", 1)
+image = cv2.imread("11.jpg", 1)
 # image = cv2.imread("1_dot.jpg", 1)
 image = cv2.resize(image, (800, 1000))
 image_original = copy.copy(image)
@@ -58,7 +87,7 @@ output = cv2.bitwise_and(image, image, mask=mask)
 ret, thresh = cv2.threshold(mask, 127, 255, 0)
 thresh = cv2.medianBlur(thresh, 11)
 
-# cv2.imshow("",thresh)
+# cv2.imshow("", thresh)
 contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
 intersectionPoints = []
@@ -78,6 +107,125 @@ if len(contours) != 0:
     cornerPoint3 = (approx[3][0][0], approx[3][0][1])
     cornerPoint4 = (approx[2][0][0], approx[2][0][1])
 
+    # persfektif görünüş
+    pts1 = np.float32([cornerPoint1, cornerPoint2, cornerPoint3, cornerPoint4])
+    pts2 = np.float32([[0, 0], [500, 0], [0, 600], [500, 600]])
+    matrix = cv2.getPerspectiveTransform(pts1, pts2)
+    perspective = cv2.warpPerspective(image, matrix, (500, 600))
+
+    cv2.imshow("Orginal perspective", perspective)
+
+    cv2.line(image, cornerPoint1, cornerPoint4, 255, 2)
+    cv2.line(image, cornerPoint3, cornerPoint2, 255, 2)
+
+    pieceX = abs(roundto((cornerPoint1[0] - cornerPoint4[0]) / 100))
+    pieceY = abs(roundto((cornerPoint1[1] - cornerPoint4[1]) / 100))
+    print("PieceX ", pieceX)
+
+    newCornerPoint1 = (0, 0)
+    prevColor = None
+    for iteration in range(0, 101, 1):
+        dot = (cornerPoint1[0] + iteration * pieceX, cornerPoint1[1] + iteration * pieceY)
+        color = image_original[dot[1], dot[0]]
+
+        curColor = color_from_rgb(color[2], color[1], color[0])
+
+        if prevColor == "red" and curColor == "white":
+            newCornerPoint1 = dot
+            break
+
+        prevColor = curColor
+
+        print(str(iteration) + "=>" + str(curColor) + "=>" + str(dot))
+
+        cv2.circle(image, dot, 1, (255, 0, 0), -1)
+        cv2.putText(image, str(iteration), dot, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+    # Corner 2
+    pieceX = abs(roundto((cornerPoint2[0] - cornerPoint3[0]) / 100))
+    pieceY = abs(roundto((cornerPoint2[1] - cornerPoint3[1]) / 100))
+    print("PieceX ", pieceX)
+
+    newCornerPoint2 = (0, 0)
+    prevColor = None
+    for iteration in range(0, 101, 1):
+        dot = (cornerPoint2[0] + iteration * pieceX, cornerPoint2[1] - iteration * pieceY)
+        color = image_original[dot[1], dot[0]]
+
+        curColor = color_from_rgb(color[2], color[1], color[0])
+
+        if prevColor == "red" and curColor == "white":
+            newCornerPoint2 = dot
+            break
+
+        prevColor = curColor
+
+        print(str(iteration) + "=>" + str(curColor) + "=>" + str(dot))
+
+        cv2.circle(image, dot, 1, (255, 0, 0), -1)
+        cv2.putText(image, str(iteration), dot, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+    pieceX = abs(roundto((cornerPoint3[0] - cornerPoint2[0]) / 100))
+    pieceY = abs(roundto((cornerPoint3[1] - cornerPoint2[1]) / 100))
+    print("PieceX ", pieceX)
+
+    newCornerPoint3 = (0, 0)
+    prevColor = None
+    for iteration in range(0, 101, 1):
+        dot = (cornerPoint3[0] - iteration * pieceX, cornerPoint3[1] + iteration * pieceY)
+        color = image_original[dot[1], dot[0]]
+
+        curColor = color_from_rgb(color[2], color[1], color[0])
+
+        if prevColor == "red" and curColor == "white":
+            newCornerPoint3 = dot
+            break
+
+        prevColor = curColor
+
+        print(str(iteration) + "=>" + str(curColor) + "=>" + str(dot))
+
+        cv2.circle(image, dot, 1, (255, 0, 0), -1)
+        cv2.putText(image, str(iteration), dot, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+    # Corner 4
+    pieceX = abs(roundto((cornerPoint4[0] - cornerPoint1[0]) / 100))
+    pieceY = abs(roundto((cornerPoint4[1] - cornerPoint1[1]) / 100))
+    print("PieceX ", pieceX)
+
+    newCornerPoint4 = (0, 0)
+    prevColor = None
+    for iteration in range(0, 101, 1):
+        dot = (cornerPoint4[0] - iteration * pieceX, cornerPoint4[1] - iteration * pieceY)
+        color = image_original[dot[1], dot[0]]
+
+        curColor = color_from_rgb(color[2], color[1], color[0])
+
+        if prevColor == "red" and curColor == "white":
+            newCornerPoint4 = dot
+            break
+
+        prevColor = curColor
+
+        print(str(iteration) + "=>" + str(curColor) + "=>" + str(dot))
+
+        cv2.circle(image, dot, 1, (255, 0, 0), -1)
+        cv2.putText(image, str(iteration), dot, cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 0), 1)
+
+    # print(newCornerPoint1)
+    # print(newCornerPoint2)
+    # print(newCornerPoint3)
+    # print(newCornerPoint4)
+    #
+    # cv2.imshow("s", image)
+    # cv2.waitKey(0)
+    # exit()
+
+    cornerPoint1 = newCornerPoint1
+    cornerPoint2 = newCornerPoint2
+    cornerPoint3 = newCornerPoint3
+    cornerPoint4 = newCornerPoint4
+
     if showCornerPoints:
         cv2.circle(image, cornerPoint1, 5, (0, 0, 255), -1)
         cv2.circle(image, cornerPoint2, 5, (0, 0, 255), -1)
@@ -90,12 +238,17 @@ if len(contours) != 0:
         cv2.line(image, cornerPoint1, cornerPoint3, 255, 2)
         cv2.line(image, cornerPoint2, cornerPoint4, 255, 2)
 
-    topLen = int(math.sqrt(abs(math.pow(cornerPoint1[0] - cornerPoint3[0], 2) - math.pow(cornerPoint1[1] - cornerPoint3[1], 2))))
-    bottomLen = int(math.sqrt(abs(math.pow(cornerPoint2[0] - cornerPoint4[0], 2) - math.pow(cornerPoint2[1] - cornerPoint4[1], 2))))
-    leftLen = int(math.sqrt(abs(math.pow(cornerPoint1[0] - cornerPoint2[0], 2) - math.pow(cornerPoint1[1] - cornerPoint2[1], 2))))
-    rightLen = int(math.sqrt(abs(math.pow(cornerPoint3[0] - cornerPoint4[0], 2) - math.pow(cornerPoint3[1] - cornerPoint4[1], 2))))
+    topLen = int(
+        math.sqrt(abs(math.pow(cornerPoint1[0] - cornerPoint3[0], 2) - math.pow(cornerPoint1[1] - cornerPoint3[1], 2))))
+    bottomLen = int(
+        math.sqrt(abs(math.pow(cornerPoint2[0] - cornerPoint4[0], 2) - math.pow(cornerPoint2[1] - cornerPoint4[1], 2))))
+    leftLen = int(
+        math.sqrt(abs(math.pow(cornerPoint1[0] - cornerPoint2[0], 2) - math.pow(cornerPoint1[1] - cornerPoint2[1], 2))))
+    rightLen = int(
+        math.sqrt(abs(math.pow(cornerPoint3[0] - cornerPoint4[0], 2) - math.pow(cornerPoint3[1] - cornerPoint4[1], 2))))
 
-    print("topLen = " + str(topLen) + ", bottomLen = " + str(bottomLen) + ", leftLen = " + str(leftLen) + ", rightLen = " + str(rightLen))
+    print("topLen = " + str(topLen) + ", bottomLen = " + str(bottomLen) + ", leftLen = " + str(
+        leftLen) + ", rightLen = " + str(rightLen))
 
     topPieceLenX = roundto((cornerPoint3[0] - cornerPoint1[0]) / 15)
     topPieceLenY = roundto((cornerPoint3[1] - cornerPoint1[1]) / 15)
@@ -150,29 +303,6 @@ if len(contours) != 0:
         if showPointDots:
             cv2.circle(image, pointDotRight, 1, (255, 0, 0), -1)
 
-    # middleTopX = int((point1[0] + point3[0]) / 2)
-    # middleTopY = int((point1[1] + point3[1]) / 2)
-    # middleTopPoint = (middleTopX, middleTopY)
-    #
-    # middleBottomX = int((point2[0] + point4[0]) / 2)
-    # middleBottomY = int((point2[1] + point4[1]) / 2)
-    # middleBottomPoint = (middleBottomX, middleBottomY)
-
-    # cv2.line(output, middleTopPoint, middleBottomPoint, 255, 2)
-    # print(middleTopPoint,middleBottomPoint)
-
-    # cv2.drawContours(image, c, -1, 255, 3)
-    # cv2.drawContours(output, c, -1, 255, 3)
-
-    # find the biggest area
-
-    # x, y, w, h = cv2.boundingRect(c)
-    # draw the book contour (in green)
-    # cv2.rectangle(output, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-# show the images
-# cv2.imshow("Result", np.hstack([image, output]))
-
 if showIndexNumbers:
     counter = 0
     for point in intersectionPoints:
@@ -182,9 +312,13 @@ if showIndexNumbers:
 
 matrix = []
 matrixRow = []
+
 for row in range(0, 15, 1):
     matrixRow = []
     for col in range(0, 15, 1):
+
+        centerColors = []
+
         current = col + row * 16
         cornerPoint1 = intersectionPoints[current]
         cornerPoint2 = intersectionPoints[current + 17]
@@ -194,16 +328,64 @@ for row in range(0, 15, 1):
         if showCenterDots:
             cv2.circle(image, (middleX, middleY), 1, (255, 0, 0), -1)
         # print(image_original[middleY, middleX])
+
         rgb = image_original[middleY, middleX]
-        matrixRow.append((rgb[0], rgb[1], rgb[2]))
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        rgb = image_original[middleY + 1, middleX + 1]
+        cv2.circle(image, (middleX + 1, middleY + 1), 1, (255, 0, 0), -1)
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        rgb = image_original[middleY - 1, middleX - 1]
+        cv2.circle(image, (middleX - 1, middleY - 1), 1, (255, 0, 0), -1)
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        rgb = image_original[middleY, middleX + 1]
+        cv2.circle(image, (middleX, middleY + 1), 1, (255, 0, 0), -1)
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        rgb = image_original[middleY, middleX - 1]
+        cv2.circle(image, (middleX, middleY - 1), 1, (255, 0, 0), -1)
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        rgb = image_original[middleY + 1, middleX]
+        cv2.circle(image, (middleX + 1, middleY), 1, (255, 0, 0), -1)
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        rgb = image_original[middleY - 1, middleX]
+        cv2.circle(image, (middleX - 1, middleY), 1, (255, 0, 0), -1)
+        centerColors.append((rgb[0], rgb[1], rgb[2]))
+
+        matrixRow.append(centerColors)
+
     matrix.append(matrixRow)
 
 # print(matrix)
 for row in matrix:
     print(row)
-# cropped = image_original[100:200, 140:440]
-# cv2.imshow("cropped", cropped)
 
+print(matrix[0][1])
+output = ""
+row_dump = []
+for row in matrix:
+    dump = []
+    for col in row:
+        player_colors = []
+        for clr in col:
+            player_colors.append(player_decision(*clr))
+
+        b = Counter(player_colors)
+        most = b.most_common(1)[0][0]
+        dump.append(str(most))
+    row_dump.append("\t".join(dump))
+
+output = "\n".join(row_dump)
+
+print(output)
+
+f = open("data.txt", "w")
+f.write(output)
+f.close()
 
 cv2.imshow('Original Image', image_original)
 cv2.imshow('Processed', image)
